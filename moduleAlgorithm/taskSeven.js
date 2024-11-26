bae7827bf41d00eeed9fb5f645f65de2f630bbe1
Vladislav Denisov <denisov.ve1997@gmail.com>
2024-11-26 12:48:34 +0400

    moduleAlgorithm taskSeven

---
+++ moduleAlgorithm/taskSeven.js
@@ -0,0 +1,19 @@
+// Я в итоге решил при помощи алгортима Кадана, у меня была ошибка
+var maximumSubarray = function (nums) {
+  // Задаем текущую сумму и результатирующую сумму
+  let curSum = 0;
+  let maxSum = nums[0];
+
+  //   Возращаем либо значение элемента, либо сумму текущего элемента и текущей суммы
+  // Возращаем либо значение текущей суммы, либо значение максимальной суммы
+  for (let i = 0; i < nums.length; i++) {
+    curSum = Math.max(nums[i], curSum + nums[i]);
+    maxSum = Math.max(curSum, maxSum);
+  }
+
+  return maxSum;
+};
+
+console.log(maximumSubarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+console.log(maximumSubarray([1]));
+console.log(maximumSubarray([5, 4, -1, 7, 8]));
--- moduleAlgorithm/taskSix.js
+++ moduleAlgorithm/taskSix.js
@@ -19,6 +19,7 @@ var twoSum = function (nums, target) {
   }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([3, 2, 4], 6));
-console.log(twoSum([3, 3], 6));
+// Раскомментировать
+// console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(twoSum([3, 3], 6));
