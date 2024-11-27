066f370ee79eb35f0b62965a5b2c0c811f9955b7
Vladislav Denisov <denisov.ve1997@gmail.com>
2024-11-27 13:57:44 +0400

    moduleBaseJS taskFive

---
+++ mobuleBaseJS/taskFive.js
@@ -0,0 +1,16 @@
+var getUniqueValuesWithoutSet = function (array) {
+  const results = [];
+
+  for (const value of array) {
+    if (!results.includes(value)) {
+      results.push(value);
+    }
+  }
+
+  return results;
+};
+
+console.log(getUniqueValuesWithoutSet([1, 2, 3, 3, 4, 5, 4, 6]));
+console.log(getUniqueValuesWithoutSet([5, 5, 5, 5, 5]));
+console.log(getUniqueValuesWithoutSet([1, 2, 3, 4, 5]));
+console.log(getUniqueValuesWithoutSet([]));
--- mobuleBaseJS/taskFour.js
+++ mobuleBaseJS/taskFour.js
@@ -4,7 +4,7 @@ const getUniqueValues = function (array) {
   return set;
 };
 
-console.log(getUniqueValues([1, 2, 3, 3, 4, 5, 4, 6]));
-console.log(getUniqueValues([5, 5, 5, 5, 5]));
-console.log(getUniqueValues([1, 2, 3, 4, 5]));
-console.log(getUniqueValues([]));
+// console.log(getUniqueValues([1, 2, 3, 3, 4, 5, 4, 6]));
+// console.log(getUniqueValues([5, 5, 5, 5, 5]));
+// console.log(getUniqueValues([1, 2, 3, 4, 5]));
+// console.log(getUniqueValues([]));
