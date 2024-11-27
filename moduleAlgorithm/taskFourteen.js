349e03757ae7c68d4fa1edeb13cfa9989ad7ea90
Vladislav Denisov <denisov.ve1997@gmail.com>
2024-11-27 09:58:39 +0400

    moduleAlgorithm taskFourteen

---
+++ moduleAlgorithm/taskFourteen.js
@@ -0,0 +1,39 @@
+var integertoRoman = function (num) {
+  // Переменная результата
+  // и массив с значением латинских чисел и римских чисел
+  let results = '';
+  const symbols = [
+    { value: 1000, symbol: 'M' },
+    { value: 900, symbol: 'CM' },
+    { value: 500, symbol: 'D' },
+    { value: 400, symbol: 'CD' },
+    { value: 100, symbol: 'C' },
+    { value: 90, symbol: 'XC' },
+    { value: 50, symbol: 'L' },
+    { value: 40, symbol: 'XL' },
+    { value: 10, symbol: 'X' },
+    { value: 9, symbol: 'IX' },
+    { value: 5, symbol: 'V' },
+    { value: 4, symbol: 'IV' },
+    { value: 1, symbol: 'I' },
+  ];
+
+  //   Первый цикл позволяет записать латинские числа,
+  // которые записываются несколькими римскими числами
+  // Если переданной число минус число в массиве больше либо равно 0,
+  // то вычитаем из переданного числа полченное число и записываем в результат первое римское число и так далее
+  for (let i = 0; i < 4; i++) {
+    for (const obj of symbols) {
+      if (num - obj.value >= 0) {
+        num = num - obj.value;
+        results += obj.symbol;
+      }
+    }
+  }
+
+  return results;
+};
+
+console.log(integertoRoman(3));
+console.log(integertoRoman(58));
+console.log(integertoRoman(1994));
--- moduleAlgorithm/taskThirteen.js
+++ moduleAlgorithm/taskThirteen.js
@@ -1,6 +1,9 @@
 var fibonacci = function (n) {
+  // Переменная результатирующего массива
   const results = [0, 1];
 
+  // Если длина массива равна 0, возращаем 0 элемент
+  // Если длинна массива равна 1, возращаем сумму первых двух элементов
   if (n === 0) {
     return results[0];
   }
@@ -11,15 +14,17 @@ var fibonacci = function (n) {
     });
   }
 
+  // Если длина равна n, заполняем массив согласно алгоритму
   for (let i = 2; i <= n; i++) {
-    results[i] = results[i - 1] + results[i - 2];
+    results.push(results[i - 1] + results[i - 2]);
   }
 
   return results[n];
 };
 
-console.log(fibonacci(0));
-console.log(fibonacci(1));
-console.log(fibonacci(2));
-console.log(fibonacci(5));
-console.log(fibonacci(10));
+// Раскомментировать
+// console.log(fibonacci(0));
+// console.log(fibonacci(1));
+// console.log(fibonacci(2));
+// console.log(fibonacci(5));
+// console.log(fibonacci(10));
